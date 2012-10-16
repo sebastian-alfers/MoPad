ffed60d501ea294fd7bafae0b6564d41fda34e16
Sebastian Alfers <sebastian.alfers@gmail.com>
2012-10-16 23:34:14 +0200

    added initial bridge, controler and game (all beta)

---
+++ bridge/server.js
@@ -0,0 +1,8 @@
+﻿var io = require('socket.io').listen(8081);
+
+io.sockets.on('connection', function (socket) {
+  socket.on('update', function (data) {
+  	console.log(data);
+    io.sockets.emit('update', { data: data });	  
+  });
+});
