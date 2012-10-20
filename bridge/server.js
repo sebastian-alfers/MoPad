30c96977debf7c0bd49ddba89c0b13a696e7c2e1
Jonas <elmos-erbe@web.de>
2012-10-20 16:31:48 +0200

    Organizational changes
    - updated to WebSocket-Node to 1.0.7
    - removed Socket.IO solution

--- bridge/server.js
+++
@@ -1,8 +0,0 @@
-﻿var io = require('socket.io').listen(8081);
-
-io.sockets.on('connection', function (socket) {
-  socket.on('update', function (data) {
-  	console.log(data);
-    io.sockets.emit('update', { data: data });	  
-  });
-});
