beb6014e742991309763c843dca74897436be80b
janinatrost <janina.trost@student.htw-berlin.de>
2012-12-02 18:26:13 +0100

    test pages

--- misc/synfony-test/symfony/src/Nina/TestBundle/Controller/TestController.php
+++ misc/synfony-test/symfony/src/Nina/TestBundle/Controller/TestController.php
@@ -4,7 +4,6 @@ namespace Nina\TestBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\RedirectResponse;
-use Acme\DemoBundle\Form\ContactType;
 
 // these import the "@Route" and "@Template" annotations
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
