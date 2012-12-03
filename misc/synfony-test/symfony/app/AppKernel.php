0e41903e01f4ae9b26e73a12ff8ad36154713aa7
janinatrost <janina.trost@student.htw-berlin.de>
2012-12-03 20:19:56 +0100

    test database connection crud

--- misc/synfony-test/symfony/app/AppKernel.php
+++ misc/synfony-test/symfony/app/AppKernel.php
@@ -25,6 +25,7 @@ class AppKernel extends Kernel
 	        new Sonata\AdminBundle\SonataAdminBundle(),
 	        new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
 	        new Nina\TestBundle\NinaTestBundle(),
+            new Acme\StoreBundle\AcmeStoreBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
