f4e7839ef4c53bf7999ea67d56414923236d91d1
Sebastian Alfers <sebastian.alfers@gmail.com>
2013-01-24 09:34:05 +0100

    jej - multiplayer draw the box

--- misc/synfony-test/symfony/web/uploads/gamejs/gamejs_Draw+The+Box_2013-01-20.js
+++ misc/synfony-test/symfony/web/uploads/gamejs/gamejs_Draw+The+Box_2013-01-20.js
@@ -7,9 +7,72 @@ $('#main').append('<br /><br /> \
 \
 ');
 
-var $playerStatus = Array();
+    var x_start = 50;
+    var y_start = 50;
+    var x = 'x';
+    var y = 'y';
 
+var playerCount = $playerPins.length;
+var colors_i = 0;    
+var colors = new Array();
+colors[colors_i++] ='F0F8FF';
+colors[colors_i++] ='ff0000';
+colors[colors_i++] ='DC143C';
+colors[colors_i++] ='00FFFF';
+colors[colors_i++] ='FAEBD7';
+colors[colors_i++] ='7FFFD4';
+colors[colors_i++] ='F0FFFF';
+colors[colors_i++] ='F5F5DC';
+colors[colors_i++] ='FFE4C4';
+colors[colors_i++] ='000000';
+colors[colors_i++] ='FFEBCD';
+colors[colors_i++] ='0000FF';
+colors[colors_i++] ='8A2BE2';
+colors[colors_i++] ='A52A2A';
+colors[colors_i++] ='DEB887';
+colors[colors_i++] ='5F9EA0';
+colors[colors_i++] ='7FFF00';
+colors[colors_i++] ='D2691E';
+colors[colors_i++] ='FF7F50';
+colors[colors_i++] ='6495ED';
+colors[colors_i++] ='FFF8DC';
+colors[colors_i++] ='00FFFF';
+colors[colors_i++] ='00008B';
+colors[colors_i++] ='008B8B';
+colors[colors_i++] ='B8860B';
+colors[colors_i++] ='A9A9A9';
+colors[colors_i++] ='A9A9A9';
+colors[colors_i++] ='006400';
+colors[colors_i++] ='BDB76B';
+colors[colors_i++] ='8B008B';
+colors[colors_i++] ='556B2F';
+colors[colors_i++] ='FF8C00';
 
+    console.log(colors);
+    console.log(colors[0]);
+    
+var playerPositions = Array();
+    var color_i = 0;
+    var color_i_max = colors.length-1;
+for(i = 0; i < playerCount; i++){
+    if(color_i > color_i_max){
+        color_i = 0;
+    }
+    else{
+        color_i = i;
+    }
+
+    playerPositions[$playerPins[i]] = new Array();
+    playerPositions[$playerPins[i]]['x'] = x_start;
+    playerPositions[$playerPins[i]]['y'] = y_start;
+    playerPositions[$playerPins[i]]['color'] = colors[color_i];
+
+    x_start += x_start;
+    y_start += y_start;
+}
+//init the start positions
+
+    console.log(playerPositions);
 
 var canvas = document.getElementById("canvas"),
     ctx = canvas.getContext("2d"),
@@ -21,16 +84,22 @@ var canvas = document.getElementById("canvas"),
 
 
 
-canvas.width = canvas.height = 600;
-ctx.fillRect(0, 0, 600, 600);
+canvas.width = 940;
+canvas.height = 600;
+ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    var x = 50;
-    var y = 50;
+$('html, body').animate({
+    scrollTop: $("#canvas").offset().top
+}, 2000);
+
+
 
     var FRAMES = 10;
 
     function drawLine(){
-        ctx.fillRect(x, y, 1 , 1 );
+        for(i = 0; i < playerCount; i++){
+            ctx.fillRect(playerPositions[$playerPins[i]]['x'], playerPositions[$playerPins[i]]['y'], 1 , 1 );
+        }
     }
 
 
@@ -40,14 +109,17 @@ function draw() {
     //ctx.clearRect(0, 0, canvas.width, canvas.height);
     //ctx.fillRect(0, 0, 600, 600);
 
-    ctx.beginPath();
-    ctx.rect(x, y, i, 1);
-    ctx.fillStyle = 'yellow';
-    ctx.fill();
+    for(i = 0; i < playerCount; i++){
+        ctx.beginPath();
+        ctx.rect(playerPositions[$playerPins[i]]['x'], playerPositions[$playerPins[i]]['y'], 1, 1);
+        ctx.fillStyle = playerPositions[$playerPins[i]]['color'];
+        ctx.fill();
+    }
 
-    ctx.lineWidth = 7;
-    ctx.strokeStyle = 'yellow';
-    ctx.stroke();
+
+    //ctx.lineWidth = 7;
+    //ctx.strokeStyle = 'yellow';
+    //ctx.stroke();
 
 }
 
@@ -68,19 +140,19 @@ $webSocketModel.on('sendCommandToGame', function(json){ // TODO move to central 
         switch(json.keycode){
            		case 'up':
                        intervalls[json.pin] = new Array();
-                       intervalls[json.pin] = window.setInterval(function(){y-=1},FRAMES)
+                       intervalls[json.pin] = window.setInterval(function(){playerPositions[json.pin]['y']-=1},FRAMES)
            			   break;
            		case 'down':
                         intervalls[json.pin] = new Array();
-                        intervalls[json.pin] = window.setInterval(function(){y+=1},FRAMES)
+                        intervalls[json.pin] = window.setInterval(function(){playerPositions[json.pin]['y']+=1},FRAMES)
            				break;
            		case 'left':
                         intervalls[json.pin] = new Array();
-                        intervalls[json.pin] = window.setInterval(function(){x-=1},FRAMES)
+                        intervalls[json.pin] = window.setInterval(function(){playerPositions[json.pin]['x']-=1},FRAMES)
            				break;
            		case 'right':
                         intervalls[json.pin] = new Array();
-                        intervalls[json.pin] = window.setInterval(function(){x+=1},FRAMES)
+                        intervalls[json.pin] = window.setInterval(function(){playerPositions[json.pin]['x']+=1},FRAMES)
            				break;
            		default: console.log('Unkown key command');
     }
