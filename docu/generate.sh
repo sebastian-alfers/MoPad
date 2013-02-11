a588cb9816c3302401ad214bc171cbcfff17e7a8
Sebastian Alfers <sebastian.alfers@gmail.com>
2013-02-11 20:41:43 +0100

    generated docu

--- docu/generate.sh
+++ docu/generate.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
-~/jsdoc/jsdoc ../gameCenter/game/paintTheBox/benchmark.js
-~/jsdoc/jsdoc ../gameCenter/game/moveTheBox/game.js
+~/jsdoc/jsdoc -d ./source_code/GameCenter/ ../gameCenter/script/models/WebSocketModel.js 
 
-~/jsdoc/jsdoc ../gameCenter/script/collections/GameCollection.js
-~/jsdoc/jsdoc ../gameCenter/script/collections/PlayerCollection.js
+~/jsdoc/jsdoc -d ./source_code/GameCenter/ ../gameCenter/game/moveTheBox/game.js
 
-~/jsdoc/jsdoc ../gameCenter/script/models/GameCenterModel.js
-~/jsdoc/jsdoc ../gameCenter/script/models/GameListModel.js
-~/jsdoc/jsdoc ../gameCenter/script/models/GameModel.js
-~/jsdoc/jsdoc ../gameCenter/script/models/PlayerModel.js
-~/jsdoc/jsdoc ../gameCenter/script/models/WebSocketModel.js
+~/jsdoc/jsdoc -d ./source_code/GameCenter/ ../gameCenter/script/collections/GameCollection.js
+~/jsdoc/jsdoc -d ./source_code/GameCenter/ ../gameCenter/script/collections/PlayerCollection.js
 
-~/jsdoc/jsdoc ../gameCenter/script/routers/gameCenterRouter.js
+~/jsdoc/jsdoc -d ./source_code/GameCenter/ ../gameCenter/script/models/GameCenterModel.js
+~/jsdoc/jsdoc -d ./source_code/GameCenter/ ../gameCenter/script/models/GameListModel.js
+~/jsdoc/jsdoc -d ./source_code/GameCenter/ ../gameCenter/script/models/GameModel.js
+~/jsdoc/jsdoc -d ./source_code/GameCenter/ ../gameCenter/script/models/PlayerModel.js
+~/jsdoc/jsdoc -d ./source_code/GameCenter/ ../gameCenter/script/models/WebSocketModel.js
 
-~/jsdoc/jsdoc ../gameCenter/script/views/GameCenterView.js
-~/jsdoc/jsdoc ../gameCenter/script/views/GameListView.js
-~/jsdoc/jsdoc ../gameCenter/script/views/PendingPlayerView.js
-~/jsdoc/jsdoc ../gameCenter/script/views/PlayerChooserView.js
-~/jsdoc/jsdoc ../gameCenter/script/views/PlayerListView.js
+~/jsdoc/jsdoc -d ./source_code/GameCenter/ ../gameCenter/script/routers/gameCenterRouter.js
 
-~/jsdoc/jsdoc ../gameCenter/script/gameCenter.js
+~/jsdoc/jsdoc -d ./source_code/GameCenter/ ../gameCenter/script/views/ErrorView.js
+~/jsdoc/jsdoc -d ./source_code/GameCenter/ ../gameCenter/script/views/GameCenterView.js
+~/jsdoc/jsdoc -d ./source_code/GameCenter/ ../gameCenter/script/views/GameListView.js
+~/jsdoc/jsdoc -d ./source_code/GameCenter/ ../gameCenter/script/views/PendingPlayerView.js
+~/jsdoc/jsdoc -d ./source_code/GameCenter/ ../gameCenter/script/views/PlayerChooserView.js
+~/jsdoc/jsdoc -d ./source_code/GameCenter/ ../gameCenter/script/views/PlayerListView.js
+~/jsdoc/jsdoc -d ./source_code/GameCenter/ ../gameCenter/script/views/WebSocketConnectionStatusView.js
 
-~/jsdoc/jsdoc ../bridge/app.js
+~/jsdoc/jsdoc -d ./source_code/GameCenter/ ../gameCenter/script/gameCenter.js
 
-~/jsdoc/jsdoc ../controller/script/script.js
-~/jsdoc/jsdoc ../controller/script/controlModules.js
-~/jsdoc/jsdoc ../controller/script/socket.js
+~/jsdoc/jsdoc -d ./source_code/GameCenter/ ../bridge/app.js
+
+~/jsdoc/jsdoc -d ./source_code/GameCenter/ ../controller/script/script.js
+~/jsdoc/jsdoc -d ./source_code/GameCenter/ ../controller/script/controlModules.js
+~/jsdoc/jsdoc -d ./source_code/GameCenter/ ../controller/script/socket.js
